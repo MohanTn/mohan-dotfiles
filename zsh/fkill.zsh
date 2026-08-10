b48896d1cd1126411f005e7ffda0a65580391919
Mohan Talkad Nagaraju <31411890+MohanTn@users.noreply.github.com>
2026-08-10 15:46:23 +0200

    Apply suggestion from @MohanTn

--- zsh/fkill.zsh
+++ zsh/fkill.zsh
@@ -1,5 +1,3 @@
-# Fuzzy-pick a listening port/service/PID from lsof and kill -9 it
-fkill() {
-  sudo lsof -i -P -n | fzf --header="Type a PORT (e.g. :8080), Service, or PID" | awk '{print $2}' | xargs -r sudo kill -9
+  sudo lsof -i -P -n -sTCP:LISTEN | fzf --header="Type a PORT (e.g. :8080), Service, or PID" | awk '{print $2}' | xargs -r sudo kill -9
   # scaffold:inject
 }
